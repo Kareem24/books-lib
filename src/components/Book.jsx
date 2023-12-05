454c07e333064a72ba168de45374b72f836e9da8
kareem roqib <kareemolakunleh@gmail.com>
2023-12-05 09:25:40 +0100

    add stylint for linting the styles and modify the linter.yml file to run lint job on pull_request and on push

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,17 +1,17 @@
 import PropTypes from "prop-types";
 function Book({ book }) {
-	const { title, author } = book;
-	return (
-		<li className="flex w-full md:w-1/2 justify-between items-center gap-4 list-none my-4">
-			<p>
-				<span>{title}</span> by <span>{author}</span>
-			</p>
-			<button className="border-2 border-black px-2 py-1">delete</button>
-		</li>
-	);
+  const { title, author } = book;
+  return (
+    <li className="flex w-full md:w-1/2 justify-between items-center gap-4 list-none my-4">
+      <p>
+        <span>{title}</span> by <span>{author}</span>
+      </p>
+      <button className="border-2 border-black px-2 py-1">delete</button>
+    </li>
+  );
 }
 Book.propTypes = {
-	book: PropTypes.object,
+  book: PropTypes.object,
 };
 
 export default Book;
