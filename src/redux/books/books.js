d6cc337b23d8d8e3a2b3ef2567f836fb1e390b2c
kareem roqib <kareemolakunleh@gmail.com>
2024-03-04 09:02:33 +0100

    add redux library and add reducer for adding and removing book from the list

---
+++ src/redux/books/books.js
@@ -0,0 +1,22 @@
+const initialState = [];
+
+function bookReducer(state = initialState, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case "book/add":
+      return [...state, payload];
+    case "book/remove":
+      return state.filter((item) => item._id !== payload);
+    default:
+      return state;
+  }
+}
+
+export const addBook = function (book) {
+  return { type: "book/add", payload: book };
+};
+export const removeBook = function (id) {
+  return { type: "book/remove", payload: id };
+};
+
+export default bookReducer;
